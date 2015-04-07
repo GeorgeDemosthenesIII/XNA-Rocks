7ab42065d3c64d4dea11dce329b63762bb108124
George <george.demosthenes@yahoo.com>
2015-04-06 21:16:17 -0400

    New Commit

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,22 @@
+using ProgrammingAssignment3;
+using System;
+
+namespace Project1
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
